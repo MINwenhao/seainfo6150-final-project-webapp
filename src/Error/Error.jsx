f832865e8c0ff6d02bb5bbd42606cb55f2cc28e5
MINwenhao <minwenhao1992@gmail.com>
2020-12-11 11:46:45 -0800

    12-11

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from "./Error.module.css";
 
 const Error = () => {
     return (
-        <div>
-            The error pages
+        <div className={styles.error}>
+            Error
+            Oops, You go where no man has gone before.
         </div>
     )
 }
